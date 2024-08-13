c383432ce767d07cb581484587800e40a51fca1c
Vita <nikaangel295@gmail.com>
2024-08-13 21:07:15 +0100

    add

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './js/projects.js';
+import './js/reviews.js';
+import './js/covers.js';
+import './js/header.js';
+import './js/work-together.js';
+import './js/swagger-api.js';
+import './js/about-me.js';
+import './js/faq.js';
+import './js/scroll.js';
+import './js/benefits.js';
+import './js/heroColors.js';
