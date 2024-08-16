995e447707740d58c5a5cfb56e7122e19ceb5d86
Vita <nikaangel295@gmail.com>
2024-08-16 15:09:58 +0100

    add change-theme

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const themeSwitcher = document.getElementById('theme-switcher');
-  const currentTheme = localStorage.getItem('theme') || 'day';
-  document.body.classList.add(currentTheme);
+// document.addEventListener('DOMContentLoaded', () => {
+//   const themeSwitcher = document.getElementById('theme-switcher');
+//   const currentTheme = localStorage.getItem('theme') || 'day';
+//   document.body.classList.add(currentTheme);
 
-  themeSwitcher.addEventListener('click', () => {
-    let newTheme = document.body.classList.contains('day') ? 'night' : 'day';
-    document.body.classList.remove('day', 'night');
-    document.body.classList.add(newTheme);
-    localStorage.setItem('theme', newTheme);
-  });
-});
+//   themeSwitcher.addEventListener('click', () => {
+//     let newTheme = document.body.classList.contains('day') ? 'night' : 'day';
+//     document.body.classList.remove('day', 'night');
+//     document.body.classList.add(newTheme);
+//     localStorage.setItem('theme', newTheme);
+//   });
+// });
