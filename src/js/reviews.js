65cb4ee36aecf5da26e3b05f7500bd1959b4b437
Vita <nikaangel295@gmail.com>
2024-08-18 15:48:31 +0100

    add review

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,20 +1,26 @@
 import Swiper from 'swiper';
 import { Navigation, Autoplay } from 'swiper/modules';
 import 'swiper/css';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const section = document.querySelector('.reviews-section');
+
 if (section) {
   const list = section.querySelector('.reviews-cards');
   const prevButton = section.querySelector('.reviews-btn-prev');
   const nextButton = section.querySelector('.reviews-btn-next');
+
   async function getReviews(value) {
     const BASE_URL = 'https://portfolio-js.b.goit.study/api';
     const END_POINT = '/reviews';
+
     const url = BASE_URL + END_POINT;
+
     try {
       const res = await fetch(url);
+
       return res.json();
     } catch (err) {
       console.log(err);
@@ -25,6 +31,7 @@ if (section) {
       return [];
     }
   }
+
   function renderReview(data) {
     return data
       .map(
@@ -40,40 +47,49 @@ if (section) {
       )
       .join('');
   }
+
   async function renderReviews() {
     try {
       const data = await getReviews();
+
       if (data.length > 0) {
         const markup = renderReview(data);
         list.insertAdjacentHTML('beforeend', markup);
+
         const $swiper = new Swiper(section.querySelector('.swiper'), {
           modules: [Navigation, Autoplay],
+
           breakpoints: {
             320: {
               slidesPerView: 1,
               loop: false,
             },
+
             768: {
               slidesPerView: 2,
               spaceBetween: 16,
               loop: false,
             },
+
             1440: {
               slidesPerView: 4,
               spaceBetween: 16,
               loop: false,
             },
           },
+
           autoplay: {
             delay: 3000,
             disableOnInteraction: true,
             pauseOnMouseEnter: true,
           },
+
           keyboard: {
             enabled: true,
             onlyInViewport: true,
             pageUpDown: true,
           },
+
           navigation: {
             nextEl: '.reviews-btn-next',
             prevEl: '.reviews-btn-prev',
@@ -90,5 +106,6 @@ if (section) {
       });
     }
   }
+
   renderReviews();
 }
