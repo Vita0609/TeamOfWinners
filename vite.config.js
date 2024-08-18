3b6616d4e21261627f9df16138fda35554155157
Subx1s0o <nazarsabardin@gmail.com>
2024-08-18 12:46:03 +0300

    restore

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import glob from 'glob';
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import FullReload from 'vite-plugin-full-reload';
 import injectHTML from 'vite-plugin-html-inject';
@@ -20,26 +19,11 @@ export default defineConfig(({ command }) => {
               return 'vendor';
             }
           },
-
-          assetFileNames: ({ name }) => {
-            if (name) {
-              const relativePath = name.includes('src/')
-                ? name.split('src/')[1]
-                : name;
-              return `assets/${relativePath}`;
-            }
-            return 'assets/[name][extname]';
-          },
           entryFileNames: 'commonHelpers.js',
         },
       },
       outDir: '../dist',
       assetsDir: 'assets',
-    },
-    resolve: {
-      alias: {
-        '@img': resolve(__dirname, 'src/img'),
-      },
     },
     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
   };
