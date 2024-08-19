b18ea85ef0d63a6065fa634e97ed923794e8333e
Vita <nikaangel295@gmail.com>
2024-08-19 22:59:07 +0100

    add

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,35 +1,35 @@
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-// import Swiper from 'swiper';
-// import { Navigation, Keyboard } from 'swiper/modules';
-// import 'swiper/css';
+import Swiper from 'swiper';
+import { Navigation, Keyboard } from 'swiper/modules';
+import 'swiper/css';
 
-// new Accordion('.about-me-accordion-container', {
-//   duration: 400,
-//   showMultiple: true,
-//   openOnInit: [0],
-// });
+new Accordion('.about-me-accordion-container', {
+  duration: 400,
+  showMultiple: true,
+  openOnInit: [0],
+});
 
-// const aboutMeSwiper = new Swiper('.about-me-swiper', {
-//   modules: [Navigation, Keyboard],
-//   loop: true,
-//   slidesPerView: 2,
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 3,
-//     },
-//     1440: {
-//       slidesPerView: 6,
-//     },
-//   },
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: false,
-//     pageUpDown: true,
-//   },
-// });
+const aboutMeSwiper = new Swiper('.about-me-swiper', {
+  modules: [Navigation, Keyboard],
+  loop: true,
+  slidesPerView: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    pageUpDown: true,
+  },
+});
 
-// document.querySelector('.swiper-button-next').addEventListener('click', e => {
-//   aboutMeSwiper.slideNext();
-// });
+document.querySelector('.swiper-button-next').addEventListener('click', e => {
+  aboutMeSwiper.slideNext();
+});
