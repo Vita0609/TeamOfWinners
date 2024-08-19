047b46d473b1e06f51ff504257ea2aeb32e40bb6
LiliiaVesrta <versta270695@gmail.com>
2024-08-19 13:32:16 +0200

    add versta

--- src/js/faq.js
+++ src/js/faq.js
@@ -4,18 +4,6 @@ new Accordion('.accordion-container-faq', {
   duration: 900,
   showMultiple: true,
 });
-
-// ОШИБКА!!! если раскоментить код, то получается
-// что текст при открытие становится в столбик
-// и как-будто прыгает, если оставить все как есть то не крутится кнопка
-
-// document.addEventListener('DOMContentLoaded', function () {
-// const acc = document.querySelectorAll('.faq-btn-icon');
-// acc.forEach(faq =>
-//   faq.addEventListener('click', () => {
-//     faq.classList.toggle('active');
-//   })
-// );
 const faqButtons = document.querySelectorAll('.btn-faq');
 
 faqButtons.forEach(button => {
@@ -27,17 +15,3 @@ faqButtons.forEach(button => {
     });
   });
 });
-//   acc.forEach(item => {
-//     const btn = item.querySelector('.btn-faq');
-//     const content = item.querySelector('.ac-panel');
-
-//     btn.addEventListener('click', function () {
-//       if (content.style.display === 'flex') {
-//         content.style.overflow = 'hidden';
-//       } else {
-//         content.style.display = 'flex';
-//       }
-//     });
-//   });
-// });
-// const faqs = document.querySelectorAll('.list-faq');
