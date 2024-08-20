1922472cb5c3f137c53b6aeccc2315350a4c4f52
Vita <nikaangel295@gmail.com>
2024-08-20 12:06:03 +0100

    add

--- src/partials/faq/js/faq.js
+++ src/partials/faq/js/faq.js
@@ -1,17 +1,23 @@
 import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import spriteUrl from '../../../img/icons.svg';
 
-new Accordion('.accordion-container-faq', {
-  duration: 900,
-  showMultiple: true,
-});
-const faqButtons = document.querySelectorAll('.btn-faq');
+document.addEventListener('DOMContentLoaded', function () {
+  new Accordion('.accordion-container', {
+    duration: 300,
+    elementClass: 'faq-item',
+    triggerClass: 'ac-trigger',
+    panelClass: 'ac-panel',
+    activeClass: 'is-active',
+    showMultiple: true,
+    beforeOpen: item => {
+      const icon = item.querySelector('.icon-answer use');
+      icon.setAttribute('href', `${spriteUrl}#icon-up`);
+    },
+    beforeClose: item => {
+      const icon = item.querySelector('.icon-answer use');
 
-faqButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const icons = button.querySelectorAll('.faq-btn-icon');
-
-    icons.forEach(icon => {
-      icon.classList.toggle('active');
-    });
+      icon.setAttribute('href', `${spriteUrl}#icon-down`);
+    },
   });
 });
