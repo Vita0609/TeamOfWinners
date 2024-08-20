a367f9a086bfb23f80f4a18e8782993ecd832944
Subx1s0o <nazarsabardin@gmail.com>
2024-08-20 17:05:55 +0300

    structure

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import 'izitoast/dist/css/iziToast.min.css';
 import './js/about-me.js';
 import './js/change-theme.js';
-import './partials/header/js/header.js';
-import './js/projects.js';
 import './js/scroll-btn.js';
 import './partials/covers/js/covers.js';
 import './partials/faq/js/faq.js';
 import './partials/footer/js/work-together.js';
+import './partials/header/js/header.js';
+import './partials/projects/js/projects';
 import './partials/reviews/js/reviews.js';
 import './swagger-api.js';
