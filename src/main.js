1bac18268e5249f6ccf7100db7febd785b3e64ff
Subx1s0o <nazarsabardin@gmail.com>
2024-08-21 17:47:37 +0300

    remove

--- src/main.js
+++ src/main.js
@@ -2,20 +2,13 @@ import 'accordion-js/dist/accordion.min.css';
 import 'izitoast/dist/css/iziToast.min.css';
 import 'swiper/css';
 import './js/change-theme.js';
-
+import './js/scroll-btn.js';
+import './partials/about-me/js/about-me.js';
+import './partials/benefits/js/benefits.js';
+import './partials/covers/js/covers.js';
+import './partials/faq/js/faq.js';
 import './partials/footer/js/work-together.js';
 import './partials/header/js/header.js';
-
-if (document.querySelector('#about-me')) {
-  import('./partials/about-me/js/about-me.js');
-}
-
-if (document.querySelector('#projects')) {
-  import('./partials/projects/js/projects');
-}
-
-import('./partials/faq/js/faq.js');
-import('./partials/covers/js/covers.js');
-import('./partials/reviews/js/reviews.js');
-
-import('./swagger-api.js');
+import './partials/projects/js/projects';
+import './partials/reviews/js/reviews.js';
+import './swagger-api.js';
